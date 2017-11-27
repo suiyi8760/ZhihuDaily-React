f663b30e6a587b7b2cbec65d56542cbc0d8acaf4
Johnny <suiyi8760@163.com>
2017-11-27 23:39:29 +0800

    fixed detail_body scroll layout&add go back

--- src/layout/DetailLayout.js
+++ src/layout/DetailLayout.js
@@ -1,11 +1,25 @@
 import {NavBar} from 'antd-mobile'
 import {Switch, Route, Redirect} from 'dva/router'
+import Iconfont from 'components/Iconfont'
 import Detail from 'routes/detail'
 
-const DetailLayout = ({match}) => {
+const DetailLayout = ({history, match}) => {
   return (
     <div>
-      <NavBar/>
+      <NavBar
+        icon={<Iconfont type="back"/>}
+        onLeftClick={e => history.goBack()}
+        rightContent={[
+          <div key={0}>
+            <Iconfont type="share" style={{marginRight: '16px'}}/>
+            <Iconfont type="collect"/>
+          </div>,
+          <div key={1}>
+            <Iconfont type="comment"/>
+            <Iconfont type="fabulous"/>
+          </div>
+        ]}
+      />
       <Switch>
         <Route path={`${match.url}/:id`} component={Detail}></Route>
         <Route exact path={`${match.url}`} component={() => <Redirect to="/"/>}></Route>
--- src/routes/detail/index.js
+++ src/routes/detail/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import {connect} from 'dva'
 import Helmet from 'react-helmet'
+import styles from './index.less'
 
 @connect(({detail}) => detail)
 class Detail extends React.Component {
+
   componentWillReceiveProps(nextProps) {
     this.lv.innerHTML = nextProps.body
   }
@@ -11,16 +13,21 @@ class Detail extends React.Component {
   render() {
 
     const {css} = this.props
-    console.log(css);
 
     return (
-      <div>
+      <div
+        className={styles.detailContent}
+        style={{height: document.documentElement.clientHeight - 45}}
+      >
         <Helmet>
           {css && css.map((item, index) => (
             <link key={index} rel="stylesheet" href={item}/>
           ))}
         </Helmet>
-        <div ref={el => this.lv = el}></div>
+        <div
+          ref={el => this.lv = el}
+          className="detailBody"
+        ></div>
       </div>
     )
   }
